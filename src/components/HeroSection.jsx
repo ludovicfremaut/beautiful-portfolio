07fd32209c09f5784e9d8c7d7b4870f43ea0301d
Ludovic Fremaut <ludovic.fremaut@hotmail.fr>
2025-05-16 16:11:46 +0200

    component: AboutSection

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,38 @@
+import { ArrowDown } from "lucide-react";
+
+export const HeroSection = () => {
+  return (
+    <section
+      id="hero"
+      className="relative min-h-screen flex flex-col items-center justify-center px-4"
+    >
+      <div className="container max-w-4xl mx-auto text-center z-10">
+        <div className="space-y-6">
+          <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+            <span className="opacity- animate-fade-in-delay-0">Hello I'm</span>{" "}
+            <span className="text-primary opacity-0 animate-fade-in-delay-1">
+              Ludovic
+            </span>{" "}
+            <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">
+              Fremaut
+            </span>{" "}
+          </h1>
+          <p className="text-lg md:text-xl text-muted-foreground max-2-2xl  mx-auto opacity-0 animate-fade-in-delay-3">
+            I create astrellar web experiences with moder technologies.
+            Specializiing in front-end development,I build interfaces that are
+            both beautifuland functional.
+          </p>
+          <div className="pt-4 opacity-0 animate-fade-in-delay-4">
+            <a href="#projects" className="cosmic-button">
+              View my work
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="absolute bottom-8 left-1/2 transform-x-1/2 flex flex-col items-center animate-bounce">
+        <span className="text-sm text-muted-foreground mb-2"> Scroll </span>
+        <ArrowDown className="h-5 w-5 text-primary" />
+      </div>
+    </section>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,28 @@
-import { ThemeToggle } from '../components/ThemeToggle';
-import { StarBackground } from '../components/StarBackground.jsx'; 
-import { NavBar } from '../components/NavBar.jsx';
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground.jsx";
+import { NavBar } from "../components/NavBar.jsx";
+import { HeroSection } from "../components/HeroSection.jsx";
+import { AboutSection } from "../components/Aboutsection.jsx";
 
 export const Home = () => {
-    return (
-        <div className="min-h-screen bg-background text-foreground overflow-x-hidden ">
-            {/* Theme Toggle */}
-            <ThemeToggle />
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden ">
+      {/* Theme Toggle */}
+      <ThemeToggle />
 
-            {/* Background Effects */}
-            <StarBackground />
+      {/* Background Effects */}
+      <StarBackground />
 
-            {/* Navbar */}
-            <NavBar />
+      {/* Navbar */}
+      <NavBar />
 
-            {/* Main Content */}
+      {/* Main Content */}
+      <main>
+        <HeroSection />
+        <AboutSection />
+      </main>
 
-            {/* Footer */}
-        </div>
-    )
-}
+      {/* Footer */}
+    </div>
+  );
+};
